d4320c3b5bedee43c48821753514a34062a59ed7
PotlaVani2002 <potlavani9194@gmail.com>
2025-08-27 17:03:42 +0530

    NEW Data has added

--- forecast-arima.py
+++ forecast-arima.py
@@ -17,7 +17,7 @@ st.set_page_config(layout="wide")
 # ---------------------------
 @st.cache_data
 def load_growth_percentage():
-    file_path = Path(__file__).parent / "new-data2.csv"
+    file_path = Path(__file__).parent / "NEW-DATA2.csv"
     df = pd.read_csv(file_path)
 
     # Ensure Date column
